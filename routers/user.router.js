3b12c87c64f2929cee17efa02b59a3bce7dd8d25
hemuu12 <harisingh.bisht2001@gmail.com>
2024-06-17 17:25:06 +0530

    added

--- routers/user.router.js
+++ routers/user.router.js
@@ -106,15 +106,6 @@ userRouter.post("/login", async (req, res) => {
 
 // user.router.js - All Request Endpoint
 
-// userRouter.get("/all-request", async (req, res) => {
-//   try {
-//     const allrequest = await requestModel.find({status:"Pending"});
-//     res.status(200).json(allrequest);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// });
 
 
 // user.router.js - Add Request Endpoint
